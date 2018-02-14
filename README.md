369c49b87fbee42cf5ae5439371d817ed04fea5f
Sandy Green <green7786@comcast.net>
2018-02-14 12:18:16 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # techdegree-project-6
-Treehouse Techdegree Project 6 - Interactive Video Player
---
+++ js/main.js
@@ -0,0 +1,30 @@
+$('video').mediaelementplayer({
+  features:  ['playpause',  'current', 'progress', 'duration', 'volume', 'fullscreen']
+});
+// Get the <video> element with id="myVideo"
+const video = document.querySelector('video');
+const span = document.getElementsByTagName('span');
+
+
+//Listen for the video to start playing
+video.addEventListener("timeupdate", () => {
+
+
+    for (i = 0; i < span.length; i += 1) //loops through span tags
+
+          if (video.currentTime > span[i].getAttribute('data-start') &&
+              video.currentTime < span[i].getAttribute('data-end')) {
+              span[i].style.color ="orange";
+          } else {
+              span[i].style.color = '#383938';
+        }
+  });
+
+
+  for (i = 0; i < span.length; i += 1) { //loops through span tags
+  // Attach a click event to the span, and execute a function if a click operation begins
+      span[i].addEventListener('click', (event) =>  {
+        video.currentTime = event.target.getAttribute('data-start');
+        video.play();
+  })
+}
