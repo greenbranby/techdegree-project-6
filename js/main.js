b77d358ad697c9b4e89c610ee2d729ad7e0f1b9b
Sandy Green <green7786@comcast.net>
2018-02-16 13:30:56 -0700

    revised comments in main.js

--- js/main.js
+++ js/main.js
@@ -13,26 +13,32 @@ const span = document.getElementsByClassName('transcript');
 
 //Listen for the video to start playing
 video.addEventListener("timeupdate", () => {
-    for (i = 0; i < span.length; i += 1) //loops through span tags
+    //loops through span tags
+    for (i = 0; i < span.length; i += 1)
 
-//Inside the loop, use a conditional to see if the video’s current time is
-//greater than the data-start attribute and less than the data-end attribute.
+          // Inside the loop, use a conditional
+          // to see if the video’s current time is
+          // greater than the data-start attribute
+          // and less than the data-end attribute.
           if (video.currentTime > span[i].getAttribute('data-start') &&
               video.currentTime < span[i].getAttribute('data-end')) {
-                
-//If the conditions are met, set the span’s color property
-//  to the highlighted color. #ff8200,
-// and if not, set the span’s color back to its initial color. #383938.
+
+              // If the conditions are met, set the span’s color property
+              //  to the highlighted color. #ff8200,
+              // and if not, set the span’s color back to its initial color. #383938.
               span[i].style.color ="#ff8200";
           } else {
               span[i].style.color = '#383938';
          }
   });
 
+ //loops through span tags
+  for (i = 0; i < span.length; i += 1) { 
 
-  for (i = 0; i < span.length; i += 1) { //loops through span tags
-  // Attach a click event to the span (transcript), when clicked anywhere
-  //on the span(transcript) player jumps to appropriate time in video and plays
+      // Attach a click event to the span (transcript),
+      // when clicked anywhere
+      // on the span(transcript) player jumps
+      // to appropriate time in video and plays
       span[i].addEventListener('click', (event) =>  {
         video.currentTime = event.target.getAttribute('data-start');
         video.play();
