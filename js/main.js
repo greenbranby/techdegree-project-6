1be4b323aa77cfa5a3c2d819bb9fee3ec55be357
Sandy Green <green7786@comcast.net>
2018-02-16 13:19:32 -0700

    Added more comments to main.js file

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $('video').mediaelementplayer({
 
 //----------Variables-----------------------
 
-// Get the <video> element with id="myVideo"
+// Get the video and transcript  elements
 const video = document.querySelector('video');
 const span = document.getElementsByClassName('transcript');
 
@@ -17,7 +17,7 @@ video.addEventListener("timeupdate", () => {
           if (video.currentTime > span[i].getAttribute('data-start') &&
               video.currentTime < span[i].getAttribute('data-end')) {
 //If the conditions are met, set the span’s color property
-//  to the highlighted color of your choice #ff8200,
+//  to the highlighted color. #ff8200,
 // and if not, set the span’s color back to its initial color. #383938.
               span[i].style.color ="#ff8200";
           } else {
@@ -27,7 +27,8 @@ video.addEventListener("timeupdate", () => {
 
 
   for (i = 0; i < span.length; i += 1) { //loops through span tags
-  // Attach a click event to the span, and execute a function if a click operation begins
+  // Attach a click event to the span (transcript), when clicked anywhere
+  //on the span(transcript) player jumps to appropriate time in video and plays 
       span[i].addEventListener('click', (event) =>  {
         video.currentTime = event.target.getAttribute('data-start');
         video.play();
