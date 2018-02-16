628c4a5aa4058ef2ab7e8f7e16a40e3a44f9051c
Sandy Green <green7786@comcast.net>
2018-02-16 13:25:17 -0700

    cleaned up my variables partial

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ $('video').mediaelementplayer({
   features:  ['playpause',  'current', 'progress', 'duration', 'volume', 'fullscreen']
 });
 
-//----------Variables-----------------------
+//----------Variables-----------------------/
 
 // Get the video and transcript  elements
 const video = document.querySelector('video');
 const span = document.getElementsByClassName('transcript');
 
 
+//--------------Event Listeners-----------/
+
 //Listen for the video to start playing
 video.addEventListener("timeupdate", () => {
     for (i = 0; i < span.length; i += 1) //loops through span tags
+
 //Inside the loop, use a conditional to see if the video’s current time is
 //greater than the data-start attribute and less than the data-end attribute.
           if (video.currentTime > span[i].getAttribute('data-start') &&
               video.currentTime < span[i].getAttribute('data-end')) {
+                
 //If the conditions are met, set the span’s color property
 //  to the highlighted color. #ff8200,
 // and if not, set the span’s color back to its initial color. #383938.
@@ -28,7 +32,7 @@ video.addEventListener("timeupdate", () => {
 
   for (i = 0; i < span.length; i += 1) { //loops through span tags
   // Attach a click event to the span (transcript), when clicked anywhere
-  //on the span(transcript) player jumps to appropriate time in video and plays 
+  //on the span(transcript) player jumps to appropriate time in video and plays
       span[i].addEventListener('click', (event) =>  {
         video.currentTime = event.target.getAttribute('data-start');
         video.play();
